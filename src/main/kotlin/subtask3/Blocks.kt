48c3fc6b9b199c5122dbd987d56148eff0a5b65b
Danya <daniil.zelenin01@mail.ru>
2021-06-20 15:20:21 +0300

    1
    Done all tasks

--- src/main/kotlin/subtask3/Blocks.kt
+++ src/main/kotlin/subtask3/Blocks.kt
@@ -1,11 +1,76 @@
 package subtask3
 
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 import kotlin.reflect.KClass
 
 class Blocks {
-
-    // TODO: Complete the following function
     fun getData(blockA: Array<*>, blockB: KClass<*>): Any {
-        throw NotImplementedError("Not implemented")
+        val onlyRequredData = getRequredData(blockA, blockB)
+        var resultInt: Int = 0
+        var resultString: String = ""
+        var resultDate: LocalDate
+
+
+        val name = blockB.java.name
+
+        if (name == "java.lang.String")
+        {
+            for(elem in onlyRequredData)
+            {
+                resultString += elem as String
+
+            }
+            return resultString
+        }
+        else if(name == "int")
+        {
+            for(elem in onlyRequredData)
+            {
+                resultInt += elem as Int
+            }
+            return resultInt
+        }
+        else if (name == "java.time.LocalDate")
+        {
+            var bufferDate: LocalDate
+            bufferDate = onlyRequredData.get(0) as LocalDate
+            for(elem in onlyRequredData)
+            {
+                if(elem as LocalDate > bufferDate)
+                {
+                    bufferDate = elem as LocalDate
+                }
+            }
+            val simpleDateFormat = DateTimeFormatter.ofPattern("dd.MM.yyyy")
+            return bufferDate.format(simpleDateFormat)
+        }
+        return -1
     }
+
+    fun getRequredData(blockA: Array<*>, blockB: KClass<*>): ArrayList<Any>{
+        var data = ArrayList<Any>(blockA.size)
+
+        when(blockB){
+            Int::class -> {
+
+                for (elem in blockA)
+                {
+                    if (elem is Int)
+                    {
+                        data.add(elem as Int)
+                    }
+                }
+                return data
+            }
+        }
+
+        data.addAll(blockA.filterIsInstance(blockB.java))
+
+
+
+        return data
+    }
+
+
 }
